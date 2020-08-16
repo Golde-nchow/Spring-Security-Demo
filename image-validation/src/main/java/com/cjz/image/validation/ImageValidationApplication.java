2cb3b6ab28a73e68fc09b1a71bc90e0adfe85a14
曹金洲 <570384231@qq.com>
2020-08-16 16:47:53 +0800

    添加图形验证码子模块

---
+++ image-validation/src/main/java/com/cjz/image/validation/ImageValidationApplication.java
@@ -0,0 +1,16 @@
+package com.cjz.image.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringSecurity + 图形验证码
+ */
+@SpringBootApplication
+public class ImageValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageValidationApplication.class, args);
+    }
+
+}
---
+++ image-validation/src/main/java/com/cjz/image/validation/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.cjz.image.validation.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/7/29 6:56
+ * @description: Security配置类
+ * @version: 1.0
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 表单提交
+        http.formLogin()
+            // 登录页面
+            .loginPage("/login")
+            // 输入 /login 就会被认为是登录操作，然后使用 UsernameAndPasswordFilter 执行表单登录，并校验逻辑
+            // 成功后默认跳转到index2
+            .loginProcessingUrl("/authentication")
+            .successForwardUrl("/index")
+            .and()
+            .authorizeRequests()
+            // 允许login.html被所有人访问
+            .antMatchers("/login*").permitAll()
+            // 其他请求需要认证
+            .anyRequest().authenticated();
+
+        // 如果不想要表单提交，则使用 http.basic() 实现弹窗登录
+
+        // 关闭 csrf 过滤
+        http.csrf().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
