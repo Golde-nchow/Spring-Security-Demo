27653f07e7334f1ccd443463bc29dcf0994f8a99
Golde-nchow <570384231@qq.com>
2020-08-18 07:40:10 +0800

    添加记住我模块

---
+++ Remember-Me/src/main/java/com/cjz/remember/me/RememberMeApplication.java
@@ -0,0 +1,19 @@
+package com.cjz.remember.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/7/28 22:40
+ * @description: 记住我的Spring-Security项目
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class RememberMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberMeApplication.class, args);
+    }
+
+}
