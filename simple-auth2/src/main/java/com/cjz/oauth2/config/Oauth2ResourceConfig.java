ebfefd167eca1ef1b93998a2481bc80082cf4a50
Golde-nchow <570384231@qq.com>
2020-09-20 00:06:29 +0800

    添加资源服务器

--- simple-auth2/src/main/java/com/cjz/oauth2/config/Oauth2ResourceConfig.java
+++ simple-auth2/src/main/java/com/cjz/oauth2/config/Oauth2ResourceConfig.java
@@ -5,10 +5,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 /**
  * @author KamChou
- * @description oauth2资源服务器，获取 access_token
+ * @description oauth2资源服务器，获取 access_token。
+ * 如果是授权码模式，需要把/oauth/authorize这个地址过滤掉，不然会出现无权限异常。
+ * 但是在密码模式当中，则不需要。
+ *
  * @date 2020/9/19 19:05
  */
 @Configuration
-//@EnableResourceServer
+@EnableResourceServer
 public class Oauth2ResourceConfig {
 }
