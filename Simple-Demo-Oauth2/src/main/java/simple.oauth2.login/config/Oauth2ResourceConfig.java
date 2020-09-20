2890a1c003259d91e78cfab9bb3d1c632498467d
Golde-nchow <570384231@qq.com>
2020-09-20 12:50:04 +0800

    创建登录成功后返回的 token，只需要输入账号密码，头部信息带上clientId和secret即可

--- Simple-Demo-Oauth2/src/main/java/simple.oauth2.login/config/Oauth2ResourceConfig.java
+++ Simple-Demo-Oauth2/src/main/java/simple.oauth2.login/config/Oauth2ResourceConfig.java
@@ -1,7 +1,13 @@
 package simple.oauth2.login.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import simple.oauth2.login.handler.MyLoginFailureHandler;
+import simple.oauth2.login.handler.MyLoginSuccessHandler;
+import simple.oauth2.login.property.SecurityProperties;
 
 /**
  * @author KamChou
@@ -13,5 +19,50 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class Oauth2ResourceConfig {
+public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private MyLoginFailureHandler myLoginFailureHandler;
+
+    @Autowired
+    private MyLoginSuccessHandler myLoginSuccessHandler;
+
+    /**
+     * 配置拦截的请求，配置和 SecurityConfig 一致
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // 获取模块抽取出来的，自定义的登录页面
+        String loginPage = securityProperties.getBrowser().getLoginPage();
+
+        // 表单提交
+        http.formLogin()
+                // 登录页面
+                .loginPage(loginPage)
+                // 输入 /login 就会被认为是登录操作，然后使用 UsernameAndPasswordFilter 执行表单登录，并校验逻辑
+                // 成功后默认跳转到index2
+                .loginProcessingUrl("/login")
+                .successForwardUrl("/index")
+                // 添加自定义的登录成功处理
+                .successHandler(myLoginSuccessHandler)
+                // 添加自定义的登录失败处理
+                .failureHandler(myLoginFailureHandler)
+                .and()
+                .authorizeRequests()
+                // 允许login.html被所有人访问
+                .antMatchers(loginPage)
+                .permitAll()
+                // 其他请求需要认证
+                .anyRequest()
+                .authenticated();
+
+        // 如果不想要表单提交，则使用 http.basic() 实现弹窗登录
+
+        // 关闭 csrf 过滤
+        http.csrf().disable();
+    }
 }
--- Simple-Demo-Oauth2/src/main/java/simple.oauth2.login/handler/MyLoginSuccessHandler.java
+++ Simple-Demo-Oauth2/src/main/java/simple.oauth2.login/handler/MyLoginSuccessHandler.java
@@ -4,13 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationConverter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * 登录成功处理类
@@ -23,6 +32,14 @@ public class MyLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    private BasicAuthenticationConverter authenticationConverter = new BasicAuthenticationConverter();
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -30,7 +47,33 @@ public class MyLoginSuccessHandler implements AuthenticationSuccessHandler {
 
         logger.info("登录成功");
 
+        // 获取认证信息
+        UsernamePasswordAuthenticationToken authRequest = authenticationConverter.convert(request);
+        if (authRequest == null) {
+            throw new UnapprovedClientAuthenticationException("请求头中无 client 信息");
+        }
+
+        // 获取 clientId 和 clientSecret
+        String username = authRequest.getName();
+        String password = (String) authRequest.getCredentials();
+        // 客户端信息
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(username);
+
+        // 身份验证
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在：" + username);
+
+        } else if (!clientDetails.getClientSecret().equals(password)) {
+            // 如果 secret 不正确，则返回
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配：" + password);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(new HashMap<>(), username, clientDetails.getScope(), "password");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        // 最终的带有 accessToken 以及其他信息的 token
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(authentication));
+        response.getWriter().write(objectMapper.writeValueAsString(oAuth2Request));
     }
 }
