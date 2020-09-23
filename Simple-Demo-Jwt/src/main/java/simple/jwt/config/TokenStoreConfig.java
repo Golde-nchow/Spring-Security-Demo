1624b3cb3f8c7bdb539ac45ad381449d9a6d2a01
Golde-nchow <570384231@qq.com>
2020-09-23 23:18:32 +0800

    添加jwt存储配置，和Redis存储方式共存

--- Simple-Demo-Jwt/src/main/java/simple/jwt/config/TokenStoreConfig.java
+++ Simple-Demo-Jwt/src/main/java/simple/jwt/config/TokenStoreConfig.java
@@ -1,10 +1,13 @@
 package simple.jwt.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
@@ -18,8 +21,22 @@ public class TokenStoreConfig {
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
 
+    /**
+     * 存储在 Redis 中
+     */
     @Bean
+    @ConditionalOnProperty(prefix = "cjz.security.oauth2", name = "storeType", havingValue = "redis")
     public TokenStore redisTokenStore() {
         return new RedisTokenStore(redisConnectionFactory);
+    }
+
+    /**
+     * 存储在 jwt 中
+     * 如果配置文件中不存在 cjz.security.oauth2.storeType = jwt 时，使用下面配置进行存储
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "cjz.security.oauth2", name = "storeType", havingValue = "jwt", matchIfMissing = true)
+    public TokenStore jwtTokenStore(JwtAccessTokenConverter converter) {
+        return new JwtTokenStore(converter);
     }
 }
