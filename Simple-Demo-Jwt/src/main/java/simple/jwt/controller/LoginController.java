8795f5a06d7a0f750180d5c5af4df4c69f5ff7c6
Golde-nchow <570384231@qq.com>
2020-09-24 23:16:12 +0800

    添加解析jwt接口

--- Simple-Demo-Jwt/src/main/java/simple/jwt/controller/LoginController.java
+++ Simple-Demo-Jwt/src/main/java/simple/jwt/controller/LoginController.java
@@ -1,11 +1,21 @@
 package simple.jwt.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.lang.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import simple.jwt.properties.SecurityProperties;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author: Kam-Chou
@@ -15,6 +25,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class LoginController {
+
+    @Autowired
+    private SecurityProperties securityProperties;
 
     /**
      * 登录页面
@@ -30,7 +43,15 @@ public class LoginController {
     @ResponseBody
     @RequestMapping("me")
     public Object getMe(Authentication authentication) {
-        return authentication;
+        // 获取token
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+        String token = details.getTokenValue();
+        // 密钥
+        String signingKey = securityProperties.getOauth2Properties().getSigningKey();
+        // 返回解析后的信息
+        return Jwts.parser()
+                .setSigningKey(signingKey.getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token).getBody();
     }
 
     /**
